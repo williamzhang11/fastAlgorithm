92f2f109873893f897b01c95d5fa5c7911f1260d
xiuyazhang <xiuyazhang@wdit.com.cn>
2019-07-24 10:04:27 +0800

    完善基数排序，经典十大排序算法总结

--- src/main/java/com/xiu/fastAlgorithm/radixsort/RadixSortTest.java
+++ src/main/java/com/xiu/fastAlgorithm/radixsort/RadixSortTest.java
@@ -1,9 +1,21 @@
 package com.xiu.fastAlgorithm.radixsort;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RadixSortTest {
-	
+	static int[] sortarray = {10,2,5,2,8,6,1,7};
 	
 	public static void main(String[] args) {
+		
+		
+		
+		int[] after = radixSort(sortarray);
+		
+		for(int a:after) {
+			System.out.println(a);
+		}
+		
 		
 	}
 	
@@ -13,18 +25,66 @@ public class RadixSortTest {
 			return array;
 		}
 		
-		//1.先算出最大数的位数
-		int max = array[0];
 		
+		int max = array[0];
+		//获取最大数
 		for(int i=1;i<array.length;i++) {
 			max = Math.max(max, array[i]);
 		}
-		
 		int maxDigit = 0;
+		//获取最大数的个位，以及位数
+		while(max != 0) {
+			max/=10;
+			maxDigit ++;
+		}
+		
+		
+		int mod = 10, div=1;
+		List<ArrayList<Integer>> bucketList = new ArrayList<ArrayList<Integer>>();
+		for(int i=0;i<10;i++) {
+			bucketList.add(new ArrayList<Integer>());
+		}
+		
+		
+		for(int i=0;i < maxDigit; i++, mod *=10, div *=10) {
+			
+			for(int j=0;j<array.length;j++) {
+				int num = (array[j]%mod)/div;
+				bucketList.get(num).add(array[j]);
+			}
+			
+			int index = 0;
+            for (int j = 0; j < bucketList.size(); j++) {
+                for (int k = 0; k < bucketList.get(j).size(); k++)
+                    array[index++] = bucketList.get(j).get(k);
+                bucketList.get(j).clear();
+            }
+
+		}
 		
 		return array;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
